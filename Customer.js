78b194cf164f5dbc3a34405582fef9af96ea9517
mjimenez05 <matthewjjimenez05@hotmail.com>
2020-04-23 19:14:23 -0600

    got the cli table to generate in my terminal. working on populating the data from sql

--- Customer.js
+++ Customer.js
@@ -1,7 +1,8 @@
-const inquirer = reqiure("inquirer");
-const mysql = require('msql');
+var Table = require("cli-table");
+var inquirer = require("inquirer")
+var mysql = require("mysql")
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 3306,
     user: "root",
@@ -11,30 +12,67 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    readProducts();
+    else console.log("\nWelcome to the store");
+    console.log("==============================");
+    start();
 });
 
-function readProducts() {
-    console.log("These Items are Available:\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        runBamazon();
-    })
-}
-
-function runBamazon() {
+function start() {
     inquirer.prompt([{
-                type: "number",
-                message: "What is the item id you're looking for?",
-                name: "id"
-            },
-            {
-                type: "number",
-                message: "How many units would you like?"
-                name: "Quantity"
-            }
-        ])
-        .then(function)
+        type: "number",
+        message: "What is the item id you're looking for?",
+        name: "confirm",
+        defalut: true
+    }]).then(function(answer) {
+        if (answer.confirm) {
+            var query = "SELECT item_id, product_name, department_id, price, stock_quantity FROM products";
+            connection.query(query, function(err, res) {
+                var table = new table({
+                    head: [
+                        "Product ID",
+                        "Prooduct Name",
+                        "Department",
+                        "Price",
+                        "Stock Quantity"
+                    ],
+                    colWidths: [20, 20, 20, 20, 20]
+                });
+                for (var i = 0; i < res.length; i++) {
+                    table.push([
+                        res[i].item_id,
+                        res[i].product_name,
+                        res[i].departmernt_id,
+                        "$" + res[i].price,
+                        res[i].stock_quantity
+                    ]);
+                }
+                console.log("\n" + table.tostring() + "\n");
+                purchaseItems();
+            });
+        } else {
+            console.log("Come on back");
+            connection.end();
+        };
+    });
+};
+
+function purchaseItems() {
+    inquirer.prompt([{
+
+    }])
 }
+// function purchaseItems(ID, quantity) {
+//     connection.query('SELECT * FROM products WHERE id = ' + ID, function(err, res) {
+//         if (err) {
+//             console.log(err);
+//         } else if (quantity <= res[0].stock_quantity) {
+//             var total = res[0].price * quantity;
+//             connection.query("UPDATE porducts SET stock_quantity = stock_quantity - " + quantity + "WHERE + ID");
+//             console.log(res[0].stock_quantity);
+//             console.log("Your total purchase is " + quantity + " " + res[0].product_name + " is " + total + " Thank you for shopping bamazon!!");
+//         } else {
+//             console.log("We are almost out of items. Whe do have " + res[0].stock_quantity + "Units Available.");
+//         };
+//         realProducts();
+//     })
+// }
--- bamazon.sql
+++ bamazon.sql
@@ -1,26 +1,26 @@
-DROP IF DATABASE EXISTS bamazon;
+DROP  DATABASE IF EXISTS bamazon;
 
 CREATE DATABASE bamazon;
 
 use bamazon;
 
 CREATE TABLE products (
-    id INT NOT NULL AUTO_INCRIMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(255)NULL,
     department_name VARCHAR(255) NULL,
     price DECIMAL(10,2) NULL,
-    stock_quantitity INIT Null,
+    stock_quantitity INT Null,
     PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantitity)
-VALUES ("Thrustmaster TSSH Sequential Handbrake & Shifter", "Electronics", 269.99),
-("Next Level Racing Motion Platform v3", "Electronics", 3000.00),
-("Thrustmaster Open Wheel Add On", "Electronics", 149.99),
-("F1 2019 Anniversary Edition - Xbox One", "Game", 59.99),
-("Thrustmaster Scuderia Ferrari Race Kit", "Electronics", 207.45),
-("ButtKicker BK-GR Gamer", "Electronics", 149.99),
-("Dirt Racing Shoe", "Clothing", 39.99),
-("Razer Baselisk Wireless Gaming Mouse", "Electronics", 59.99),
-("I Paused My Game to Be Here T-Shirt", "Clothing", 269.99),
-("L Shaped Desk Corner Computer Desk", "Furniature", 159.99)
+VALUES ("Thrustmaster TSSH Sequential Handbrake & Shifter", "Electronics", 269.99, 5),
+("Next Level Racing Motion Platform v3", "Electronics", 3000.00,8),
+("Thrustmaster Open Wheel Add On", "Electronics", 149.99, 10),
+("F1 2019 Anniversary Edition - Xbox One", "Game", 59.99, 20),
+("Thrustmaster Scuderia Ferrari Race Kit", "Electronics", 207.45, 3),
+("ButtKicker BK-GR Gamer", "Electronics", 149.99, 16),
+("Dirt Racing Shoe", "Clothing", 39.99, 0),
+("Razer Baselisk Wireless Gaming Mouse", "Electronics", 59.99, 6),
+("I Paused My Game to Be Here T-Shirt", "Clothing", 269.99, 25),
+("L Shaped Desk Corner Computer Desk", "Furniature", 159.99, 7)
